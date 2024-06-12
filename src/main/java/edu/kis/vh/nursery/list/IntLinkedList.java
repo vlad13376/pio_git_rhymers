dcd9a97396ccf02a396305d60179b6bd00882938
vlad13376 <115876929+vlad13376@users.noreply.github.com>
2024-06-12 18:52:46 +0200

    przeniesienie klasy Node, #14

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,39 @@ package edu.kis.vh.nursery.list;
 import static edu.kis.vh.nursery.DefaultCountingOutRhymer.DEFAULT_NEGATIVE_VALUE;
 public class IntLinkedList {
 
+ public static class Node {
+
+        private final int value;
+        private Node prev;
+        private Node next;
+
+        public Node(int i) {
+            value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+    }
+
+    private final int LINKED_LIST_DEFAULT_NEGATIVE_VALUE = -1;
     Node last;
     int i;
 
